83536459c053ee6324f064dbb7e376ae40895e70
Draken1509 <huutrung368@gmail.com>
2023-07-13 15:43:34 +0700

    init project

---
+++ body/Sidebar.php
@@ -0,0 +1,97 @@
+<div class="sidebar sidebar-style-2">			
+			<div class="sidebar-wrapper scrollbar scrollbar-inner">
+				<div class="sidebar-content">
+					<div class="user">
+						<div class="avatar-sm float-left mr-2">
+							<img src="assets/img/account.png" alt="..." class="avatar-img rounded-circle">
+						</div>
+						<div class="info">
+							<a data-toggle="collapse" href="#collapseExample" aria-expanded="true">
+								<span>
+									Tài khoản
+								</span>
+                                <h5>Tên nhân viên</h5>
+							</a>
+							
+						</div>
+					</div>
+					<ul class="nav nav-primary">
+						<li class="nav-item active">
+							<a data-toggle="collapse" href="GiaoDien_Admin_Lan3.php" class="collapsed" aria-expanded="false">
+								<i class="fas fa-home"></i>
+								<p>Trang chủ</p>
+								
+							</a>
+							
+						</li>
+						<li class="nav-section">
+							<span class="sidebar-mini-icon">
+								<i class="fa fa-ellipsis-h"></i>
+							</span>
+							<h4 class="text-section">Danh mục quản lý</h4>
+						</li>
+						<li class="nav-item">
+							<a href="admin_SanPham.php">
+								<i class="fas fa-coins"></i>
+								<p>Sản phẩm</p>									  							
+							</a>
+							
+						</li>
+                        <li class="nav-item">
+							<a  href="admin_KhachHang.php">
+								<i class="fas fa-people-carry"></i>
+								<p>Khách Hàng</p>							
+							</a>
+							
+						</li>
+                        <li class="nav-item">
+							<a data-toggle="collapse" href="#base">
+								<i class="fas fa-cart-plus"></i>
+								<p>Đơn Hàng</p>
+								<span class="caret"></span>
+							</a>
+							
+						</li>
+                        <li class="nav-item">
+							<a  href="admin_NhapHang.php">
+								<i class="fas fa-cart-arrow-down"></i>
+								<p>Nhập Hàng</p>								
+							</a>
+							
+						</li>
+						<li class="nav-item">
+							<a  href="admin_dsDonHangNhap.php">
+								<i class="fas fa-cart-arrow-down"></i>
+								<p>DS đơn nhập hàng</p>								
+							</a>							
+						</li>
+						<li class="nav-item">
+							<a  href="admin_LSmuacuaKh.php">
+								<i class="fas fa-cart-arrow-down"></i>
+								<p>DS đơn mua hàng</p>								
+							</a>							
+						</li>
+                        <li class="nav-item">
+							<a data-toggle="collapse" href="#base">
+								<i class="fas fa-file-contract"></i>
+								<p>Báo cáo</p>
+								<span class="caret"></span>
+							</a>
+							
+						</li>
+                        <li class="nav-item">
+							<a data-toggle="collapse" href="#base">
+								<i class="fas fa-chart-bar"></i>
+								<p>Thống Kê</p>
+								<span class="caret"></span>
+							</a>
+							
+						</li>
+						
+						<li class="mx-4 mt-2">
+							<a href="TrangChu.php" class="btn btn-primary btn-block"><span class="btn-label mr-2"> <i class="fas fa-hand-point-left"></i> </span>Trang chính</a> 
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
---
+++ header.php
@@ -0,0 +1,45 @@
+
+<header class="header">
+
+   <div class="header-1">
+      <div class="flex">
+         <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+            <a href="#" class="fab fa-twitter"></a>
+            <a href="#" class="fab fa-instagram"></a>
+            <a href="#" class="fab fa-linkedin"></a>
+         </div>
+         <p><a href="DangNhap.php">Đăng nhập</a> | <a href="DangKy.php">Đăng ký</a> </p>
+      </div>
+   </div>
+
+   <div class="header-2">
+      <div class="flex">
+         <a href="TrangChu.php" class="logo">BOOK SHOP FAKE</a>
+
+         <nav class="navbar">
+            <a href="TrangChu.php">Trang Chủ</a>
+            <a href="SanPham.php">Sản Phẩm</a>
+            <a href="#">Giới Thiệu</a>
+            <a href="#">Liên Hệ</a></a>
+            <a href="#">Tin Tức</a>
+         </nav>
+
+         <div class="icons">
+            <div id="menu-btn" class="fas fa-bars"></div>            
+            <a href="#" class="fas fa-search">
+            <input type="text" name="search" placeholder="Nhập sản phẩm cần tìm..." class="box">             
+            </a>
+            <div id="user-btn" class="fas fa-user"></div>            
+            <a href="cart.php"> <i class="fas fa-shopping-cart"></i></a>
+         </div>
+
+         <div class="user-box">
+            <p>Tên tài khoản : <span></span></p>
+            <p>Email : <span></span></p>
+            <a href="DangNhap.php" class="delete-btn">Đăng Xuất</a>
+         </div>
+      </div>
+   </div>
+
+</header>
